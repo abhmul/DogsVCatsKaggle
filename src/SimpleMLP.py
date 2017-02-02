0369b16f5c4dd191a44b193d5be1278aa2de3dbe
abhmul <abhmul@gmail.com>
2017-02-01 22:54:50 -0600

    Added some documentation

--- src/SimpleMLP.py
+++ src/SimpleMLP.py
@@ -5,13 +5,20 @@ import dogsvcats
 
 Xtr, ytr = dogsvcats.load_train('../input/train/', (64, 64), grayscale=True)
 # Flattens the input vectors
+# TODO Comment the line below if passing to a convnet
 Xtr = Xtr.reshape(Xtr.shape[0], -1)
 
 model = Sequential()
+### TODO Replace this code here with a ConvNet architecture ###
 model.add(Dense(200, activation='relu', input_dim=Xtr.shape[1]))
 model.add(Dense(200, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
+### TODO Replace the above code here with a ConvNet architecture ###
 
 model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
+# Mess around with the number of epochs to make training better
 fit = model.fit(Xtr, ytr, nb_epoch=10, validation_split=.2)
+
+# TODO (Abhijeet) add an early stopping callback
+# TODO (Abhijeet) add a create_submission function
--- src/dogsvcats.py
+++ src/dogsvcats.py
@@ -6,6 +6,13 @@ from keras.preprocessing.image import img_to_array
 
 
 def read_image(file_path, img_size=None, grayscale=False):
+    """
+    Reads an image from a filepath with specified settings
+    :param file_path: path to the image
+    :param img_size: size image should be loaded to
+    :param grayscale: Whether or not image should be grayscale
+    :return: A PIL Image object
+    """
     # Do some typechecking
     if img_size is not None and ((not isinstance(img_size, tuple)) or len(img_size) != 2):
         raise TypeError("img_size must be a tuple of (w, h); given %s" % img_size)
@@ -15,6 +22,7 @@ def read_image(file_path, img_size=None, grayscale=False):
         img = img.convert('L')
     else:
         img = img.convert('RGB')
+    # Resize if we need to
     if img_size is not None:
         img = img.resize(img_size, Image.BICUBIC)
 
@@ -22,12 +30,22 @@ def read_image(file_path, img_size=None, grayscale=False):
 
 
 def prep_data(images, img_size=(64, 64), grayscale=False):
+    """
+    Prepares a list of image paths into a 4d numpy array
+    :param images: an iterable of image paths
+    :param img_size: The size each image should be loaded to
+    :param grayscale: Whether or not each image should be grayscale
+    :return: a 4d numpy array of shape (num_images, img_height, img_width, num_channels)
+    """
     count = len(images)
+    # Grayscale has just one channel, RGB has 3
     if grayscale:
         channels = 1
     else:
         channels = 3
+    # Initialize the numpy array that will hold all the image data
     data = np.empty((count, img_size[1], img_size[0], channels), dtype=np.uint8)
+    # Iterate through each image, turn it into a numpy array, and insert it into the data numpy array
     for i, image_file in enumerate(images):
         image = read_image(image_file, img_size=img_size, grayscale=grayscale)
         data[i] = img_to_array(image)
@@ -37,14 +55,30 @@ def prep_data(images, img_size=(64, 64), grayscale=False):
 
 
 def load_train(train_dir='../input/train/', img_size=(64, 64), grayscale=False, shuffle=True):
+    """
+    Loads the training dataset
+    :param train_dir: the directory the training images are stored
+    :param img_size: The size to load the image to
+    :param grayscale: Whether or not image should be grayscale
+    :param shuffle: Whether or not to shuffle the dataset before returning
+    :return: A tuple of the image 4d numpy array and a 1d array of binary labels
+    """
     X_list = []
     y_list = []
+    # For each class load the image data
+    # i = 0 for cat, i = 1 for dog
     for i, class_name in enumerate(('cat', 'dog')):
+        # Get a list of filepaths for images of the specific class
         img_fpaths = [os.path.join(train_dir, fname) for fname in os.listdir(train_dir) if class_name in fname]
+        # Add the image data to our list of numpy array data
         X_list.append(prep_data(img_fpaths, img_size=img_size, grayscale=grayscale))
+        # Add the labels to our label list
         y_list.append(np.full((len(img_fpaths),), float(i), dtype=float))
+    # Merge the dog and cat numpy arrays
+    # Normalize the pixel values in the image to be in [0,1]
     X = np.vstack(X_list) / 255.
     y = np.hstack(y_list)
+    # Shuffle the data if we need to
     if shuffle:
         inds = np.arange(X.shape[0])
         np.random.shuffle(inds)
@@ -54,11 +88,19 @@ def load_train(train_dir='../input/train/', img_size=(64, 64), grayscale=False, 
 
 
 def load_test(test_dir='../input/test/', img_size=(64, 64), grayscale=False):
+    """
+    Loads the test data
+    :param test_dir: the directory the test images are stored
+    :param img_size: The size to load the image to
+    :param grayscale: Whether or not image should be grayscale
+    :return: A 4d numpy array of image data
+    """
     img_fpaths = [os.path.join(test_dir, fname) for fname in os.listdir(test_dir)]
     return prep_data(img_fpaths, img_size=img_size, grayscale=grayscale) / 255.
 
 
 if __name__ == "__main__":
+    # Just some informal testing stuff
     IMG_SIZE = (32, 32)
     for g in (True, False):
         Xtr, ytr = load_train(img_size=IMG_SIZE, grayscale=g)
